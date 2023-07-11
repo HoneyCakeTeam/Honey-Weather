17d228b859c7ec6f63643f3d154b35569ac4884b
Ethaar hussien <ethaar7735@gmail.com>
2023-07-11 10:28:03 +0300

    design system

--- src/jvmMain/kotlin/ui/theme/Color.kt
+++ src/jvmMain/kotlin/ui/theme/Color.kt
@@ -1,2 +1,10 @@
 package ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val backgroundColor = Color(0xFFF1F1F1)
+val CardColor = Color(0xFAFAFAFA)
+val TextColor87 = Color(0xDE000000)
+val TextColor60 = Color(0x99000000)
+val TextColor37 = Color(0x4D000000)
+
--- src/jvmMain/kotlin/ui/theme/Type.kt
+++ src/jvmMain/kotlin/ui/theme/Type.kt
@@ -1,2 +1,30 @@
 package ui.theme
 
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val titleLarge = TextStyle(
+    fontFamily = Robot,
+    fontWeight = FontWeight.Bold,
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+)
+
+val label = TextStyle(
+    fontFamily = Robot,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    lineHeight = 16.sp,
+    letterSpacing = 0.5.sp
+)
+
+val labelSmall = TextStyle(
+    fontFamily = Robot,
+    fontWeight = FontWeight.Normal,
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+    letterSpacing = 0.5.sp
+)
