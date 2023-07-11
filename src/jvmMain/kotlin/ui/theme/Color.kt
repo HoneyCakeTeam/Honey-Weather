16899800fe6ee856cc44d1a53c2e66026a4f0cc0
Ethaar hussien <ethaar7735@gmail.com>
2023-07-12 01:13:54 +0300

    chang name color text

--- src/jvmMain/kotlin/ui/theme/Color.kt
+++ src/jvmMain/kotlin/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val backgroundColor = Color(0xFFF1F1F1)
 val CardColor = Color(0xFAFAFAFA)
-val TextColor87 = Color(0xDE000000)
-val TextColor60 = Color(0x99000000)
-val TextColor37 = Color(0x4D000000)
+val Black87 = Color(0xDE000000)
+val Black60 = Color(0x99000000)
+val Black37 = Color(0x4D000000)
 
--- src/jvmMain/kotlin/ui/theme/Type.kt
+++ src/jvmMain/kotlin/ui/theme/Type.kt
@@ -1,7 +1,6 @@
 package ui.theme
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -9,6 +8,7 @@ val titleLarge = TextStyle(
     fontFamily = Robot,
     fontWeight = FontWeight.Bold,
     fontSize = 22.sp,
+    color = Black87,
     lineHeight = 28.sp,
     letterSpacing = 0.sp
 )
@@ -17,6 +17,7 @@ val label = TextStyle(
     fontFamily = Robot,
     fontWeight = FontWeight.Medium,
     fontSize = 18.sp,
+    color = Black60,
     lineHeight = 16.sp,
     letterSpacing = 0.5.sp
 )
@@ -25,6 +26,7 @@ val labelSmall = TextStyle(
     fontFamily = Robot,
     fontWeight = FontWeight.Normal,
     fontSize = 11.sp,
+    color = Black37,
     lineHeight = 16.sp,
     letterSpacing = 0.5.sp
 )
