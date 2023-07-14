6dd6547b1e8bbb3e3e7e827b6f81e36d0b7232a2
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-14 16:43:57 +0300

    add text

--- src/jvmMain/kotlin/ui/composable/BodyContent.kt
+++ src/jvmMain/kotlin/ui/composable/BodyContent.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ui.theme.BackgroundColor
+import ui.theme.Typography
 
 @Composable
 fun BodyContent() {
@@ -21,6 +23,8 @@ fun BodyContent() {
         SearchBar(modifier = Modifier.fillMaxWidth())
         Spacer(modifier = Modifier.height(16.dp))
         MainCard()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Today Highlights", style = Typography.h1)
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
