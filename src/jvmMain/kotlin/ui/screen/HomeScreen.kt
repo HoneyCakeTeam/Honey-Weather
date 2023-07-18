4b2598430ef43cb2fbd93d822888c39fc7b74a0b
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-18 10:30:28 +0300

    apply loading state with animation

--- src/jvmMain/kotlin/ui/screen/HomeScreen.kt
+++ src/jvmMain/kotlin/ui/screen/HomeScreen.kt
@@ -1,16 +1,23 @@
 package ui.screen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import org.koin.java.KoinJavaComponent.inject
 import ui.composable.BodyContent
 import ui.composable.CustomDivider
+import ui.composable.LoadingAnimation
 import ui.composable.SideBar
 import ui.theme.BackgroundColor
 import ui.viewmodel.WeatherUiState
@@ -27,11 +34,29 @@ fun HomeScreen() {
 fun HomeContent(
     state: WeatherUiState
 ) {
-    Box(modifier = Modifier.fillMaxSize().background(color = BackgroundColor)) {
-        Row {
-            SideBar(state)
-            CustomDivider()
-            BodyContent(state)
+    if (state.isLoading) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LoadingAnimation()
+        }
+    } else if (state.isError) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
+    } else {
+        Box(modifier = Modifier.fillMaxSize().background(color = BackgroundColor)) {
+            Row {
+                SideBar(state)
+                CustomDivider()
+                BodyContent(state)
+            }
         }
     }
 }
